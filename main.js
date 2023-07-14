2a32d7de3d085f4a284a1cb458a08b419002f0c3
yeongmins <oxox123456@naver.com>
2023-07-14 21:16:32 +0900

    update?

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
-// 데이터 타입 확인
+// 산술(Arithmetic)
 
-console.log(typeof 'Hello' === 'string')
-console.log(typeof 123 === 'number')
-console.log(typeof false === 'boolean')
-console.log(typeof undefined === 'undefined')
-console.log(typeof null === 'object')
-console.log(typeof [] === 'object')
-console.log(typeof {} === 'object')
-console.log(typeof function () {} === 'function')
-
-// console.log(null.constructor)
-console.log([].constructor === Array)
-console.log({}.constructor === Object)
-
-function checkType(data) {
-    return Object.prototype.toString.call(data).slice(8, -1)
-}
-
-console.log(checkType('Hello') === 'String')
-console.log(checkType(123) === 'Number')
-console.log(checkType(false) === 'Boolean')
-console.log(checkType(undefined) === 'Undefined')
-console.log(checkType(null) === 'Null')
-console.log(checkType([]) === 'Array')
-console.log(checkType({}) === 'Object')
-console.log(checkType(function () {}) === 'Function')
+console.log(1 + 2)
+console.log(5 - 7)
+console.log(3 * 4)
+console.log(10 / 2)
+console.log(7 % 5)
