ee32950c07d82cd5b3f1a67692765d4504772cd0
yeongmins <oxox123456@naver.com>
2023-07-15 13:21:44 +0900

    update

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ console.log(5 - 7)
 console.log(3 * 4)
 console.log(10 / 2)
 console.log(7 % 5)
+
+function isEven(num) {
+    return num % 2 === 0
+}
+
+console.log(isEven(3))
+console.log(isEven(12))
