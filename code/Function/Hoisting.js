017f6abf2f2eb035fdab1afa351ed157d5b2cee4
yeongmins <oxox123456@naver.com>
2023-07-18 19:04:14 +0900

    update

---
+++ code/Function/Hoisting.js
@@ -0,0 +1,15 @@
+// 함수 (Function)
+
+// 함수 선언문 (Declaration)
+// function hello() {}
+
+// 함수 표현식 (Expression)
+// const hello = function() {}
+
+// 호이스팅 (Hoisting)
+
+const world = function hello() {
+    console.log('Hello~')
+}
+
+world()
