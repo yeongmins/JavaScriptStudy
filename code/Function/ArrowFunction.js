1942c3bff8cbe977416421291ffa61d1af5ca5f8
yeongmins <oxox123456@naver.com>
2023-07-22 14:25:33 +0900

    update

--- code/Function/ArrowFunction.js
+++ code/Function/ArrowFunction.js
@@ -1,1 +1,14 @@
 // 화살표 함수(Arrow function)
+
+// function sum() {}
+// const sum = function () {}
+// const sum = () => {}
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+const sum = (a, b) => a + b
+
+console.log(sum(1, 2)) // 3
+console.log(sum(10, 20)) // 30
